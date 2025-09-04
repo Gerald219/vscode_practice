7066975dfa513fb99d619fed5c530af133a36ee2
Gerald Mulero <gdmasm@gmail.com>
2025-09-04 11:57:08 -0400

    exercise 2 practice for pythonmm, input game - this asks name/age and print future age.

---
+++ 2_input_game.py
@@ -0,0 +1,9 @@
+# Ask for user's name and age, then calculate future age
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+# Converts the age into a number and calculate in 5 years
+future_age = int(age) + 5
+
+
+print(f"Hi {name}! In 5 years, you’ll be really old, like {future_age} years old.")
